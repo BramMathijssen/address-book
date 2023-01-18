3c352ab4dd4dbaab49a2b4c7b055c50ea0bb92cd
Bram <Bram.mathijssen@gmail.com>
2023-01-18 23:35:38 +0100

    address book project setup

---
+++ address-book/hardhat/hardhat.config.js
@@ -0,0 +1,24 @@
+require("ethers");
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-deploy");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.7",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      forking: {
+        url: "",
+      },
+    },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    user: {
+      default: 1,
+    },
+  },
+};
