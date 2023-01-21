cc96a747970583568a28b1d1e76b4d3390ecb982
Bram <Bram.mathijssen@gmail.com>
2023-01-21 16:37:47 +0100

    added edit functionality in the modal

--- address-book/addressbook-frontend/components/EditModal.js
+++ address-book/addressbook-frontend/components/EditModal.js
@@ -1,9 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import ReactModal from "react-modal";
+import EthersContext from "../context/ethers-context";
 
 import styles from "./EditModal.module.scss";
 
 const EditModal = (props) => {
+  const [address, setAddress] = useState("");
+  const [name, setName] = useState("");
+  const ethersCtx = useContext(EthersContext);
+
+  const editHandler = async (e) => {
+    e.preventDefault();
+    await ethersCtx.contract.editAddress(address, name);
+  };
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleAddressChange = (e) => {
+    setAddress(e.target.value);
+  };
 
   return (
     <div>
@@ -24,6 +41,13 @@ const EditModal = (props) => {
       >
         <h1>Edit</h1>
         <p>Edit the Address here</p>
+        <form onSubmit={editHandler}>
+          <label>Name</label>
+          <input type="text" onChange={handleNameChange}></input>
+          <label>Address</label>
+          <input type="text" onChange={handleAddressChange}></input>
+          <button>Edit</button>
+        </form>
         <button onClick={props.closeModal}>Close Modal</button>
       </ReactModal>
     </div>
