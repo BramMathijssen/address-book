d387b11943261c00cc209cafd394ef7e60da1e53
Bram <Bram.mathijssen@gmail.com>
2023-04-14 09:46:29 +0200

    added support for goerli

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -2,22 +2,37 @@ require("@nomiclabs/hardhat-ethers");
 require("hardhat-deploy");
 require("dotenv").config();
 
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
+
 module.exports = {
-  solidity: "0.8.7",
-  networks: {
-    hardhat: {
-      chainId: 31337,
-      // forking: {
-      //   url: "",
-      // },
+    solidity: "0.8.7",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+        },
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
+        },
     },
-  },
-  namedAccounts: {
-    deployer: {
-      default: 0,
+    goerli: {
+        url: GOERLI_RPC_URL,
+        accounts: [PRIVATE_KEY],
+        chainId: 5,
     },
-    user: {
-      default: 1,
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        user: {
+            default: 1,
+        },
     },
-  },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
 };
